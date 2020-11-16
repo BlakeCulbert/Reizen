da6f68334944e1394720aa9f92e13ac0e96f2534
Blake Culbert <blake.culbert@cune.org>
2020-11-15 20:28:52 -0600

    Implemented firebase storage for images

--- Reizen/MarkerViewController.swift
+++ Reizen/MarkerViewController.swift
@@ -7,25 +7,64 @@
 
 import UIKit
 import FirebaseDatabase
+import FirebaseStorage
 
 class MarkerViewController: UIViewController {
     
-    @IBOutlet weak var markerXLabel: UILabel!
+
+    @IBOutlet weak var hikeTitle: UILabel!
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var hikeDescription: UILabel!
+    
+    @IBOutlet weak var length: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let markerX = UserDefaults.standard.float(forKey: "Key")
         
-        let ref = Database.database().reference()
+        let dataRef = Database.database().reference()
+        
+        let storageRef = Storage.storage().reference()
+        
         
         switch markerX {
         case -2.1261265:
-            print("hello")
-            // markerXLabel.text = "Wonderland Trail"
-            ref.child("WonderlandTrail/description").observeSingleEvent(of: .value, with: { (snapshot) in
-                self.markerXLabel.text = snapshot.value as? String
-                
+            
+            let imageList = ["images/wonderland1.jpg", "images/wonderland2.jpg"]
+            
+            dataRef.child("WonderlandTrail/title").observeSingleEvent(of: .value, with: { (snapshot) in
+                self.hikeTitle.text = snapshot.value as? String
+            })
+            
+            var imgCount = 0
+            
+            for item in imageList {
+                let sRef = storageRef.child(item)
+                sRef.getData(maxSize: 1 * 1024 * 1024, completion: { (data, error) in
+                    if error != nil {
+                        print("Image could not download!")
+                    }
+                    if imgCount == 0 {
+                        let image = UIImage(data: data!)
+                        self.image1.image = image
+                    }
+                    else {
+                        let image = UIImage(data: data!)
+                        self.image2.image = image
+                    }
+                    imgCount = 1
+                    
+                })
+            }
+            
+            dataRef.child("WonderlandTrail/description").observeSingleEvent(of: .value, with: { (snapshot) in
+                self.hikeDescription.text = snapshot.value as? String
+            })
+            
+            dataRef.child("WonderlandTrail/length").observeSingleEvent(of: .value, with: { (snapshot) in
+                self.length.text = snapshot.value as? String
             })
             
         default:
