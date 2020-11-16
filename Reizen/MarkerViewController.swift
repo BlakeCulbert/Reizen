aef5b0de7a9952cef9e43c1bc6e6d19bbb3abe79
Blake Culbert <blake.culbert@cune.org>
2020-11-15 23:54:28 -0600

    getHikeData function added, uses both Firebase Realtime Database (description, length, title) and Firebase Storage (images)

--- Reizen/MarkerViewController.swift
+++ Reizen/MarkerViewController.swift
@@ -16,61 +16,71 @@ class MarkerViewController: UIViewController {
     @IBOutlet weak var image1: UIImageView!
     @IBOutlet weak var image2: UIImageView!
     @IBOutlet weak var hikeDescription: UILabel!
-    
     @IBOutlet weak var length: UILabel!
+    
+    let dataRef = Database.database().reference()
+    let storageRef = Storage.storage().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let markerX = UserDefaults.standard.float(forKey: "Key")
         
-        let dataRef = Database.database().reference()
+        // let dataRef = Database.database().reference()
         
-        let storageRef = Storage.storage().reference()
+        // let storageRef = Storage.storage().reference()
         
         
         switch markerX {
         case -2.1261265:
             
             let imageList = ["images/wonderland1.jpg", "images/wonderland2.jpg"]
+            let hikeName = "WonderlandTrail"
+            getHikeData(hikeName: hikeName, imageList: imageList)
             
-            dataRef.child("WonderlandTrail/title").observeSingleEvent(of: .value, with: { (snapshot) in
-                self.hikeTitle.text = snapshot.value as? String
-            })
-            
-            var imgCount = 0
-            
-            for item in imageList {
-                let sRef = storageRef.child(item)
-                sRef.getData(maxSize: 1 * 1024 * 1024, completion: { (data, error) in
-                    if error != nil {
-                        print("Image could not download!")
-                    }
-                    if imgCount == 0 {
-                        let image = UIImage(data: data!)
-                        self.image1.image = image
-                    }
-                    else {
-                        let image = UIImage(data: data!)
-                        self.image2.image = image
-                    }
-                    imgCount = 1
-                    
-                })
-            }
-            
-            dataRef.child("WonderlandTrail/description").observeSingleEvent(of: .value, with: { (snapshot) in
-                self.hikeDescription.text = snapshot.value as? String
-            })
-            
-            dataRef.child("WonderlandTrail/length").observeSingleEvent(of: .value, with: { (snapshot) in
-                self.length.text = snapshot.value as? String
-            })
             
         default:
             print("no")
         }
         // Do any additional setup after loading the view.
+    }
+    
+    
+    private func getHikeData(hikeName: String, imageList: [String]) {
+        
+        dataRef.child("\(hikeName)/title").observeSingleEvent(of: .value, with: { (snapshot) in
+            self.hikeTitle.text = snapshot.value as? String
+        })
+        
+        var imgCount = 0
+        
+        for item in imageList {
+            let sRef = storageRef.child(item)
+            sRef.getData(maxSize: 1 * 1024 * 1024, completion: { (data, error) in
+                if error != nil {
+                    print("Image could not download!")
+                }
+                if imgCount == 0 {
+                    let image = UIImage(data: data!)
+                    self.image1.image = image
+                }
+                else {
+                    let image = UIImage(data: data!)
+                    self.image2.image = image
+                }
+                imgCount = 1
+                
+            })
+        }
+        
+        dataRef.child("\(hikeName)/description").observeSingleEvent(of: .value, with: { (snapshot) in
+            self.hikeDescription.text = snapshot.value as? String
+        })
+        
+        dataRef.child("\(hikeName)/length").observeSingleEvent(of: .value, with: { (snapshot) in
+            self.length.text = snapshot.value as? String
+        })
+        
     }
     
     /*
